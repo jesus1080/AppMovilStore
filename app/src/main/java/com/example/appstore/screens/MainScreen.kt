b505ca8a3d5f24071e9a77b32b2b6cdd080d1cc6
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-10-29 12:30:15 -0500

    init commit

---
+++ app/src/main/java/com/example/appstore/screens/MainScreen.kt
@@ -0,0 +1,17 @@
+package com.example.appstore.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen (){
+
+}
+
+@Composable
+fun BodyContent(){
+    Column {
+
+    }
+}
