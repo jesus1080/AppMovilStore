1c94233956085743b7099e2cb9b7aa1e86a62e20
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-10-29 14:31:24 -0500

    feat: add icon to top bar

--- app/src/main/java/com/example/appstore/screens/MainScreen.kt
+++ app/src/main/java/com/example/appstore/screens/MainScreen.kt
@@ -2,20 +2,26 @@ package com.example.appstore.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
+import java.time.format.TextStyle
 
 
 @Composable
@@ -55,7 +61,32 @@ fun BodyContent(modifier: Modifier){
         Text(text = "Van los productos")
     }
 }
+@Composable
+fun ContentTopBar(){
+    var text by remember { mutableStateOf(TextFieldValue()) }
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            textStyle = androidx.compose.ui.text.TextStyle(color = Color.Black),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .padding(8.
+                dp),
+            singleLine = true
+        ) {
+            
+        }
 
+    }
+}
 @Preview
 @Composable
 fun PreviewMain(){
