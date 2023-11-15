d9ed136a075fe6ed88590cd145de5a9827e155c2
victorsotelo45 <victormsm@unicauca.edu.co>
2023-11-15 16:55:20 -0500

    fix: add imports

--- app/src/main/java/com/example/appstore/screens/SuccessfulBuy.kt
+++ app/src/main/java/com/example/appstore/screens/SuccessfulBuy.kt
@@ -1,4 +1,37 @@
-val myColor = Color(0.1f, 0.7f, 0.1f, 0.7f)
+package com.example.appstore.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.ColorUtils
+import androidx.navigation.NavController
+import com.example.appstore.navigation.StoreScreens
+
+
+@Composable
+fun SuccessfulBuy(navController: NavController){
+    val myColor = Color(0.1f, 0.7f, 0.1f, 0.7f)
     val mycolor = Color(0.9f, 0.8f, 0.9f, 1.0f)
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -37,15 +70,16 @@ val myColor = Color(0.1f, 0.7f, 0.1f, 0.7f)
     }
 
 
+}
 @Composable
 fun InfoText(text: String){
 
-        Text(
-            text = text,
-            fontFamily = FontFamily.SansSerif,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            color = Color.Black.copy(alpha = 0.4f)
-        )
+    Text(
+        text = text,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center,
+        color = Color.Black.copy(alpha = 0.4f)
+    )
 
 }
